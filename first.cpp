fe6c4e21a7d90ce7a9be637dd1a9e85dd603f68c
sharif <sharif542m@gmail.com>
2021-05-12 13:18:41 +0600

    commint from linux

---
+++ first.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std ;
+#define ll long long int
+int main()
+{
+    int t ;
+    cin>>t ;
+    while(t--)
+    {
+        ll x ;
+        cin>>x ;
+        ll k=x-1 ;
+
+        cout<<1<<" " <<k<<endl ;
+    }
+}
